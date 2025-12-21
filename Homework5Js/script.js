344a3b2ffdc21cdafcf1d935f6adc90a52bb1151
damjanmitov8 <damjanmitov8@gmail.com>
2025-12-21 15:55:01 +0100

    Add files via upload

---
+++ Homework5Js/script.js
@@ -0,0 +1,13 @@
+let phone = 119.95
+let tax = 5 
+let PhoneNumber = 30
+let precent = 100
+let taxprice = phone * tax / precent
+console.log(taxprice)
+
+let tax1 = 5.9975
+let OnePhonePrice = phone + tax1 
+console.log(OnePhonePrice)
+
+let PhonePrice = OnePhonePrice * PhoneNumber
+console.log(PhonePrice)
